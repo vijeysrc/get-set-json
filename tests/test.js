acab1cf81724fa8d22622ca43e02a897c2caadee
Vijey N <vijeysrc@gmail.com>
2019-10-04 11:45:21 +0530

    used toBe

--- tests/test.js
+++ tests/test.js
@@ -3,22 +3,22 @@ import { data_1 } from './data'
 
 describe('Get - the main getter function', () => {
   test('should get the value at the specified path for a given object', () => {
-    expect(get(data_1, ['a', 'b', 'c', 'd', 'e', 'f1'])).toEqual('Hello')
+    expect(get(data_1, ['a', 'b', 'c', 'd', 'e', 'f1'])).toBe('Hello')
   })
 
   test('should return the specified default value if get operation fails', () => {
-    expect(get(data_1, ['a', 'b', 'c', 'dx', 'e', 'f1'], 'N/A')).toEqual('N/A')
+    expect(get(data_1, ['a', 'b', 'c', 'dx', 'e', 'f1'], 'N/A')).toBe('N/A')
   })
 
   test('should return null on failure when the default value is not specified', () => {
-    expect(get(data_1, ['a', 'b', 'c', 'dx', 'e', 'f1'])).toEqual(null)
+    expect(get(data_1, ['a', 'b', 'c', 'dx', 'e', 'f1'])).toBe(null)
   })
 })
 
 describe('Set - the main setter function', () => {
   test('should set value at the specified path immutably', () => {
     const setObj = set(data_1, ['a', 'b', 'c', 'd', 'e', 'f1'], 'Greetings!!!')
-    expect(get(data_1, ['a', 'b', 'c', 'd', 'e', 'f1'])).toEqual('Hello')
-    expect(get(setObj, ['a', 'b', 'c', 'd', 'e', 'f1'])).toEqual('Greetings!!!')
+    expect(get(data_1, ['a', 'b', 'c', 'd', 'e', 'f1'])).toBe('Hello')
+    expect(get(setObj, ['a', 'b', 'c', 'd', 'e', 'f1'])).toBe('Greetings!!!')
   })
 })
