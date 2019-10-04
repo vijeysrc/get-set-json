69bd5651268417a36cbb80aebae4e43b9ec97a16
Vijey N <vijeysrc@gmail.com>
2019-10-04 13:05:39 +0530

    set refactor

--- src/index.js
+++ src/index.js
@@ -5,25 +5,29 @@ const get = (obj, keys = [], returnOnFailureValue = null) =>
       (acc, key) => (acc && acc[key] ? acc[key] : returnOnFailureValue),
       obj
     ),
-  set = (obj, path = [], value) => {
-    return path.reverse().reduce((acc, curr, index, data) => {
-      const headList = data.slice(index + 1).reverse(),
-        setLevel = get(obj, headList)
+  set = (inputObject, path = [], value) =>
+    path
+      .reverse()
+      .reduce((result, currPathItem, currPathIndex, givenPathReversed) => {
+        const headList = givenPathReversed.slice(currPathIndex + 1).reverse(),
+          setLevel = get(inputObject, headList)
 
-      if (isObject(setLevel)) {
-        return {
-          ...setLevel,
-          [curr]: acc
+        if (isObject(setLevel)) {
+          return {
+            ...setLevel,
+            [currPathItem]: result
+          }
         }
-      }
-      if (isArray(setLevel)) {
-        const keyAsNum = +curr
 
-        return Object.assign([...setLevel], {
-          [keyAsNum]: acc
-        })
-      }
-    }, value)
-  }
+        if (isArray(setLevel) && !isNaN(currPathItem)) {
+          const keyAsNum = +currPathItem
+
+          return Object.assign([...setLevel], {
+            [keyAsNum]: result
+          })
+        }
+
+        return result
+      }, value)
 
 export { get, set }
